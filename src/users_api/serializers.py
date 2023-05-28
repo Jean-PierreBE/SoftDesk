9a39c94ddb5d53cdae4aa3df9da3f9a37c7d85c5
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-05-28 20:12:35 +0200

    3ème commit

---
+++ src/users_api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from users_api import models
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'pseudo', 'first_name', 'last_name', 'email', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            pseudo=validated_data['pseudo'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- src/users_api/views.py
+++ src/users_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
+from users_api import serializers
+from users_api import models
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('pseudo', 'first_name', 'last_name', 'email',)
