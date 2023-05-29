31666f5844508ae1b8bd9cbb80aab46fcc31e309
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-05-29 21:57:56 +0200

    5ème commit

--- src/users_api/admin.py
+++ src/users_api/admin.py
@@ -2,9 +2,4 @@ from django.contrib import admin
 from users_api.models import UserProfile
 # Register your models here.
 
-
-class UserAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(UserProfile, UserAdmin)
+admin.site.register(UserProfile)
