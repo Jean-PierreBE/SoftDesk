30e4ae3058be7210539025e9f6dac0870130d300
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-05-30 22:50:15 +0200

    7ème commit

--- src/tracking/admin.py
+++ src/tracking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from tracking.models import Projects, Contributor, Issues, Comments
 # Register your models here.
+
+admin.site.register(Projects)
+
+admin.site.register(Contributor)
+
+admin.site.register(Issues)
+
+admin.site.register(Comments)
