74549adc7d0835d4eafb550f5789794f6fa2f45c
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-08-10 22:06:31 +0200

    8ème commit

--- src/tracking/urls.py
+++ src/tracking/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from tracking.views import ProjectView, ContributorView, IssueView
+from tracking.views import ProjectView, ContributorView, IssueView, CommentView
 
 router = DefaultRouter()
 router.register('project', ProjectView, basename='project')
 router.register('contributor', ContributorView, basename='contributor')
-router.register('project/<pk>/issue', IssueView, basename='project-detail')
+router.register('project/(?P<project_id>[^/.]+)/issue', IssueView, basename='issue')
+router.register('project/(?P<project_id>[^/.]+)/issue/(?P<issue_id>[^/.]+)/comment', CommentView, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls))
--- src/tracking/views.py
+++ src/tracking/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from tracking.models import Project, Contributor, Issue
-from tracking.serializers import ContributorSerializer, ProjectSerializer, ProjectDetailSerializer, IssueSerializer
+from tracking.models import Project, Contributor, Issue, Comment
+from tracking.serializers import ContributorSerializer, ProjectSerializer, \
+    ProjectDetailSerializer, IssueSerializer, CommentSerializer
 from tracking.permissions import UpdateOwnObjects
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -26,7 +27,7 @@ class ProjectView(viewsets.ModelViewSet):
 
         project = serializer.save()
 
-        self.contributor.user = self.request.user
+        self.contributor.author_user = self.request.user
         self.contributor.role = "CRT"
         self.contributor.project_id = project.id
         self.contributor.save()
@@ -36,7 +37,27 @@ class ContributorView(viewsets.ModelViewSet):
     serializer_class = ContributorSerializer
     permission_classes = [IsAuthenticated, UpdateOwnObjects]
 
+    def get_queryset(self):
+        return self.queryset.filter(author_user=self.request.user)
+
 class IssueView(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
     permission_classes = [IsAuthenticated, UpdateOwnObjects]
+
+    def get_queryset(self):
+        return self.queryset.filter(project_id=self.kwargs["project_id"])
+
+    def perform_create(self, serializer):
+        serializer.save(project_id=self.kwargs["project_id"],author_user=self.request.user)
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, UpdateOwnObjects]
+
+    def get_queryset(self):
+        return self.queryset.filter(issue_id=self.kwargs["issue_id"])
+
+    def perform_create(self, serializer):
+        serializer.save(issue_id=self.kwargs["issue_id"],author_user=self.request.user)
