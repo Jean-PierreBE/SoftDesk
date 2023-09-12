ceceff81143ddc7f445d94751d3eb3ffb7028783
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-09-12 22:11:28 +0200

    add controle error bis

--- src/tracking/permissions.py
+++ src/tracking/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 from tracking.models import Project, Contributor
 from django.db.models import Q
-from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
 
 class UpdateOwnObjects(permissions.BasePermission):
@@ -19,14 +19,10 @@ class CreateIssueComment(permissions.BasePermission):
     """Allow user to edit their own objects"""
 
     def has_permission(self, request, view):
-        """Check user is trying to edit their own objects"""
-        try:
-            parent_project = Project.objects.get(pk=view.kwargs['project_id'])
-            user_project = Contributor.objects.filter(Q(project_id=parent_project.id) &
-                                                      Q(author_user=request.user))
-            if user_project.exists():
-                return True
-            else:
-                return False
-        except:
-            raise ValidationError(detail="project_id doesn'st exist")
+        parent_project = get_object_or_404(Project, pk=view.kwargs['project_id'])
+        user_project = Contributor.objects.filter(Q(project_id=parent_project.id) &
+                                                  Q(author_user=request.user))
+        if user_project.exists():
+            return True
+        else:
+            return False
